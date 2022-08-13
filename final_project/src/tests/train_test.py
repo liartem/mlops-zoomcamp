2af269fec51a910d13eba72fa4781d804fb48e77
Artem <leeartem95@gmail.com>
2022-08-13 12:48:34 +0000

    added unit test

---
+++ final_project/src/tests/train_test.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from train import make_label_encoding, train_model
+from pandas.testing import assert_frame_equal
+
+
+def test_make_label_encoding():
+    
+    object = pd.DataFrame({"Gender": "Male", "Age": 20, "AnnualSalary": 1000}, index=[0])
+    actual_result = make_label_encoding(object)
+    
+    expected_result = pd.DataFrame({"Gender": 0, "Age": 20, "AnnualSalary": 1000}, index=[0])
+    assertion = assert_frame_equal(actual_result,  expected_result)
+    assert None == assertion
+
