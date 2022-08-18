92b6e08dd1d7053e948dcd4dc7fd4a9002b87a45
Artem <leeartem95@gmail.com>
2022-08-18 15:59:37 +0000

    modified cd pipeline

--- final_project/src/send_data.py
+++ final_project/src/send_data.py
@@ -9,7 +9,7 @@ import requests
 
 table = pd.read_csv("./evidently_service/datasets/test.csv")
 table = pa.Table.from_pandas(table, preserve_index=True)
-data  = table.to_pylist() 
+data  = table.to_pylist()
 
 
 class DateTimeEncoder(json.JSONEncoder):
